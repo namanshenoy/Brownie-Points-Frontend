e3b9a15b2601bf32a6239619b3dd737e5e8e6804
Caleb <chialeilee@umass.edu>
2018-01-28 07:07:23 -0500

    no

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -13,50 +13,14 @@
           <v-text-field v-model="password" type="password" placeholder="password"></v-text-field>
 
           <div class="flex-c">
-            <v-btn type="submit" class="pure-button pure-button-primary" v-on:click="create">Create</v-btn>
-            <v-btn type="submit" class="pure-button pure-button-primary" v-on:click="cancel">Cancel</v-btn>
+            <v-btn class="pure-button pure-button-primary" v-on:click="create">Create</v-btn>
+            <v-btn class="pure-button pure-button-primary" v-on:click="cancel">Cancel</v-btn>
           </div>
 
         </v-form>
       </v-flex>
     </v-layout>
   </v-content>
-  <!--
-  <div class="col-md-12 createAccount" id="createAccount">
-    <form class="pure-form pure-form-stacked" v-on:submit.prevent="doLogin" id="form">
-      <fieldset>
-        <legend>Create Account</legend>
-
-        <div class="row">
-          <label for="fname">First Name</label>
-          <input id="fname" v-model="fname" type="text" placeholder="First Name">
-        </div>
-        <div class="row">
-          <label for="lname">Last Name</label>
-          <input id="lname" v-model="lname" type="text" placeholder="Last Name">
-        </div>
-        <div class="row">
-          <label for="username">Username</label>
-          <input id="username" v-model="username" type="text" placeholder="Username">
-        </div>
-        <div class="row">
-          <label for="password">Password</label>
-          <input id="password" v-model="password" type="password" placeholder="Password">
-        </div>
-        <div class="row">
-          <label for="email">E-mail</label>
-          <input id="email" v-model="email" type="text" placeholder="E-mail">
-        </div>
-        <div class="row">
-          <label for="email-conf">Confirm E-mail</label>
-          <input id="email-conf" v-model="econf" type="text" placeholder="Confirm E-mail">
-        </div>
-        <button type="submit" class="pure-button pure-button-primary" v-on:click="create">Create</button>
-        <button type="submit" class="pure-button pure-button-primary" v-on:click="cancel">Cancel</button>
-      </fieldset>
-    </form>
-  </div>
-  -->
 </template>
 
 <script>
